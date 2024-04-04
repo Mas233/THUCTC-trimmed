69fc560e11395eabe69a2540c02e77698c20fe53
wang mingsong <211250143@smail.nju.edu.cn>
2024-04-04 17:30:32 +0800

    version 1.0

---
+++ THUCTC.md
@@ -0,0 +1,7 @@
+# THUCTC-trimmed
+## Introduction
+This is a trimmed version of [THUCTC](http://thuctc.thunlp.org/), a Chinese text classifying tool by THUNLP lab. 
+This version is mainly focused on news classification, using the trained model from the original THUCTC.
+
+## Acknowledgement
+**Zhipeng Guo, Yu Zhao, Yabin Zheng, Xiance Si, Zhiyuan Liu, Maosong Sun. THUCTC: An Efficient Chinese Text Classifier. 2016.**
---
+++ src/org/thunlp/language/english/EnglishBigramWordSegment.java
@@ -0,0 +1,44 @@
+package org.thunlp.language.english;
+
+import org.thunlp.language.chinese.WordSegment;
+
+import java.util.LinkedList;
+
+public class EnglishBigramWordSegment implements WordSegment {
+
+    private LinkedList<String> results = null;
+
+    private boolean withSpaceInBigram;
+
+    public EnglishBigramWordSegment() {
+        this(false);
+    }
+
+    public EnglishBigramWordSegment(boolean b) {
+        results = new LinkedList<String>();
+        withSpaceInBigram = b;
+    }
+
+    public boolean outputPosTag() {
+        return false;
+    }
+
+    public String[] segment(String text) {
+        EnglishWordSegment seg = new EnglishWordSegment();
+        String[] tokens = seg.segment(text);
+        int len = tokens.length;
+        results.clear();
+        if (len > 1) {
+            for (int i = 0; i < len - 1; ++i)
+                if (withSpaceInBigram)
+                    results.add(tokens[i] + " " + tokens[i + 1]);
+                else
+                    results.add(tokens[i] + tokens[i + 1]);
+            return results.toArray(new String[results.size()]);
+        } else if (len < 1) {
+            return new String[]{""};
+        } else {
+            return tokens;
+        }
+    }
+}
